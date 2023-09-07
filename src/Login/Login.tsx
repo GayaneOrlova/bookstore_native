e829642a89313d5f4d8b886a3362ed786b3d483c
Гаяне <gayanev.stepanyan@yandex.ru>
2023-09-07 11:04:52 +0300

    working with Login Page and useForm

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,17 +1,67 @@
 import React from 'react';
-import {useNavigation} from '@react-navigation/native';
-import {View, TouchableOpacity, Image, TextInput, Button, Text} from 'react-native';
+import { Controller, useForm } from "react-hook-form";
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, TouchableOpacity, Image, TextInput, Button, Text } from 'react-native';
 import Header from '../Header/Header';
 import LoginStyles from './LoginStyles';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useAppDispatch } from '../store/hooks';
 
 const Login = () => {
- 
+  const dispatch = useAppDispatch();
+
+  const { control, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = async () => {
+    try {
+      // const response = await postUser({email, password});
+      // await AsyncStorage.setItem('access', response.data.tokens.access);
+      // dispatch(setUser(response.data.user));
+    } catch (er) {
+      console.log(er);
+    }
+  };
+
   return (
     <View>
       <Header />
       <View style={LoginStyles.login_container}>
         <Text style={LoginStyles.login_title}>Log In</Text>
+
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              placeholder="Email"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="Email"
+        />
+        {errors.Email && <Text>This is required.</Text>}
+
+        <Controller
+          control={control}
+          rules={{
+            maxLength: 100,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="Last name"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="lastName"
+        />
+
+        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
       </View>
     </View>
   );
